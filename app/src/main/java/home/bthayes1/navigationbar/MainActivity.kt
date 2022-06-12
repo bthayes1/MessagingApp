4b937e15bcb34830081cb50d27744af8bc96caaf
bthayes1 <79590455+bthayes1@users.noreply.github.com>
2022-06-12 07:18:37 -0400

    Initial commit

---
+++ app/src/main/java/home/bthayes1/navigationbar/MainActivity.kt
@@ -0,0 +1,11 @@
+package home.bthayes1.navigationbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
